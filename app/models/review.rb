e6fa00cf7453973e31577ae02871f15338c80230
MaciejJaromin <maciej_jaromin@o2.pl>
2014-10-18 14:05:59 +0200

    More meaningful validations.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ActiveRecord::Base
   validates :title, :score, :content, presence: true
+  validates :score, numericality: {only_integer: true, less_than_or_equal_to: 100, greater_than_or_equal_to: 0, message: "must be an integer between 0 and 100"}
+  validates :title, length:{maximum: 100}
+  validates :content, length:{minimum: 25}
 end
